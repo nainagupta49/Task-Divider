c973c7c6cfb89e048cb58a395a1c38e5f34788bb
AMANJI <amanjiofficial@gmail.com>
2020-04-20 13:46:25 +0530

    Required Files

---
+++ lottery-react/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+
+class App extends Component {
+	
+  state = {
+	  manager: ''
+  };
+
+
+  async componentDidMount() {
+	  const manager = await lottery.methods.manager().call();
+	  
+	  this.setState({ manager });
+  }
+  render() {
+  return (
+	  <div>
+	  <h2>Lottery Contract</h2>
+	  <p>This contract is managed by {this.state.manager}</p>
+	</div>
+  );
+	  
+  }
+}
+
+export default App;
